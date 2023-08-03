09e3d3e087560e10707b722586f0eae7e49e51d4
devprodigio <vasconcellos_2008@outlook.com>
2023-08-03 19:52:06 -0300

    get

--- server.js
+++ server.js
@@ -1,5 +1,18 @@
+/* GET
+
 const express = require('express')
 
 const app = express()// inicializando express
 
 app.listen('3000') // ouvindo o navegador
+
+app.route('/').get( (req,res) => res.send('Olá') )
+*/
+
+// PUT
+
+const express = require('express')
+
+const app = express()// inicializando express
+
+app.listen('3000') 
